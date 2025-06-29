22120fdf85264e2e21b3e68ca06f9abc171b3c37
Mouleeswaran-0920 <mouleeswaranm.cse2024@citchennai.net>
2025-06-29 17:05:42 +0530

    Fix tsparticles loadSlim import error

--- src/components/ParticleBackground.tsx
+++ src/components/ParticleBackground.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 import Particles from 'react-particles';
-import { loadSlim } from 'tsparticles';
+import { loadSlim } from '@tsparticles/slim';
 import type { Container, Engine } from 'tsparticles';
 
 interface ParticleBackgroundProps {
